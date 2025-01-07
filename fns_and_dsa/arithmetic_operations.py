6b716bc51f269d51b9831043c1949abf926cce10
ABAYA12 <123239306+ABAYA12@users.noreply.github.com>
2025-01-07 11:14:05 +0000

    added file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,11 +7,10 @@ def perform_operation(num1, num2, operation):
         elif operation == "multiply":
             return num1 * num2
         elif operation == "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
+            if num2 == 0:
                 return "Error: Division by zero is not allowed."
+            return num1 / num2
         else:
-            return "Error: Unknown operation. Please use add, subtract, multiply, or divide."
+            return "Error! Unknown operation. Please use add, subtract, multiply, or divide."
     except Exception as e:
         return f"Error: {str(e)}"
