bdc585908f991668dc3122fe1050f354075174e3
ABAYA12 <123239306+ABAYA12@users.noreply.github.com>
2025-01-13 08:03:39 +0000

    added Files

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,7 @@
-# temp_conversion_tool.py
 
 # Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
 def convert_to_celsius(fahrenheit):
