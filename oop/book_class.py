f4e4c3d6ea237b3e7cffb47876eb96023752aad6
Ishmael Kabu Abayateye <123239306+ABAYA12@users.noreply.github.com>
2025-01-30 08:37:45 +0000

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,19 @@
 class Book:
-        def __init__(self, title, author, year):
-                """Constructor to initialize a Book instance."""
-                        self.title = title
-                                self.author = author
-                                        self.year = year
+    def __init__(self, title, author, year):
+        """Constructor to initialize a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
 
-                                            def __str__(self):
-                                                    """Retrns a user-friendly string representation."""
-                                                            return f"{self.title} by {self.author}, published in {self.year}"
+    def __str__(self):
+        """Returns a user-friendly string representation."""
+        return f"{self.title} by {self.author}, published in {self.year}"
 
-                                                                def __repr__(self):
-                                                                        """Returns an official string representation for object recreation."""
-                                                                                return f"Book('{self.title}', '{self.author}', {self.year})"
+    def __repr__(self):
+        """Returns an official string representation for object recreation."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
 
-                                                                                    def __del__(self):
-                                                                                            """Prints a message when a Book instance is deleted."""
-                                                                                                    print(f"Deleting {self.title}")
+    def __del__(self):
+        """Prints a message when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
 
